ee3a0337ac6f4fec65764545850f5338dd071411
fukayayuto <fukaya1287@gmail.com>
2020-06-24 22:06:35 +0900

    Initial Commit

---
+++ GitHub2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub2
+//
+//  Created by 深谷祐斗 on 2020/06/24.
+//  Copyright © 2020 yuto fukaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
